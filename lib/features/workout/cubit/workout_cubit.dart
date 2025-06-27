693e625f2d334730e982b5962a62ddfa0ed4ab8a
berke.yz <berkeyilmaz189@gmail.com>
2025-06-27 18:26:38 +0300

    .

--- lib/features/workout/cubit/workout_cubit.dart
+++ lib/features/workout/cubit/workout_cubit.dart
@@ -1,4 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:superset/core/models/exercise.dart';
+import 'package:superset/core/models/muscle_group.dart';
+import 'package:superset/core/models/workout.dart';
+import 'package:superset/core/models/workout_log.dart';
 import 'package:superset/core/service/exercise_service.dart';
 import 'package:superset/features/workout/cubit/workout_state.dart';
 
@@ -30,5 +34,46 @@ final class WorkoutCubit extends Cubit<WorkoutState> {
         ),
       );
     }
+  }
+
+
+  void createWorkout(){
+    final workout= Workout(
+      date: state.selectedDate,
+      logs: [
+        WorkoutLog(
+          date: DateTime.now(),
+          exercise: Exercise(
+            muscleGroup: MuscleGroup.chest,
+            name: 'Bench Press',
+
+          ),
+          reps: 10,
+          sets: 3,
+          weight: 60,
+        ),
+        WorkoutLog(
+          date: DateTime.now(),
+          exercise: Exercise(
+            muscleGroup: MuscleGroup.lats,
+            name: 'Pull Up',
+          ),
+          reps: 8,
+          sets: 3,
+          weight: 0,
+        ),
+        WorkoutLog(
+          date: DateTime.now(),
+          exercise: Exercise(
+            muscleGroup: MuscleGroup.shoulders,
+            name: 'Shoulder Press',
+          ),
+          reps: 12,
+          sets: 3,
+          weight: 40,
+        ),
+      ],
+      note: 'Bu antrenman çok güzeldir',
+    );
   }
 }
--- lib/features/workout/cubit/workout_state.dart
+++ lib/features/workout/cubit/workout_state.dart
@@ -1,11 +1,13 @@
 import 'package:equatable/equatable.dart';
 import 'package:superset/core/models/exercise.dart';
+import 'package:superset/core/models/workout.dart';
 
 final class WorkoutState extends Equatable {
   const WorkoutState({
     required this.selectedDate,
     this.isLoading = false,
     this.exercises,
+    this.workout,
   });
 
   //ilk açıldıgında selectedDate'i bugünün tarihi olarak ayarlıyoruz
@@ -15,20 +17,28 @@ final class WorkoutState extends Equatable {
 
   final bool isLoading;
   final DateTime selectedDate;
+
+  /// This is used for all exercises for the app.
   final List<Exercise>? exercises;
 
+  ///tarihi seçtiğinde firebase'e istek atılacak eğer o tarihte bir workout varsa o gelecek
+  /// eğer yoksa add exercise butonuna basınca yeni bir workout oluşturulacak
+  final Workout? workout;
+
   @override
-  List<Object?> get props => [selectedDate, exercises, isLoading];
+  List<Object?> get props => [selectedDate, exercises, isLoading, workout];
 
   WorkoutState copyWith({
     DateTime? selectedDate,
     List<Exercise>? exercises,
     bool? isLoading,
+    Workout? workout,
   }) {
     return WorkoutState(
       selectedDate: selectedDate ?? this.selectedDate,
       exercises: exercises ?? this.exercises,
       isLoading: isLoading ?? this.isLoading,
+      workout: workout ?? this.workout,
     );
   }
 }
