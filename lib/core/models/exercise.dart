9616a5b9d0c9e6b9f99d2c31167cd1ca3e1b2feb
berke.yz <berkeyilmaz189@gmail.com>
2025-06-27 14:56:52 +0300

    models updated

--- lib/core/models/exercise.dart
+++ lib/core/models/exercise.dart
@@ -4,42 +4,30 @@ import 'package:superset/core/models/muscle_group.dart';
 final class Exercise with EquatableMixin {
   Exercise({
     this.name,
-    this.sets,
-    this.rep,
-    this.weight,
+
     this.muscleGroup,
   });
 
   factory Exercise.fromJson(Map<String, dynamic> json) {
     return Exercise(
       name: json['name'] as String?,
-      sets: json['sets'] as int?,
-      rep: json['rep'] as int?,
-      weight: json['weight'] as double?,
       muscleGroup: json['muscleGroup'] as MuscleGroup?,
     );
   }
   final String? name;
-  final int? sets;
-  final int? rep;
-  final double? weight;
   final MuscleGroup? muscleGroup;
 
   @override
-  List<Object?> get props => [name, sets, rep, weight, muscleGroup];
+  List<Object?> get props => [name, muscleGroup];
 
   Exercise copyWith({
     String? name,
-    int? sets,
-    int? rep,
-    double? weight,
+
     MuscleGroup? muscleGroup,
   }) {
     return Exercise(
       name: name ?? this.name,
-      sets: sets ?? this.sets,
-      rep: rep ?? this.rep,
-      weight: weight ?? this.weight,
+
       muscleGroup: muscleGroup ?? this.muscleGroup,
     );
   }
--- lib/core/models/workout.dart
+++ lib/core/models/workout.dart
@@ -1,34 +1,40 @@
 import 'package:equatable/equatable.dart';
-import 'package:superset/core/models/exercise.dart';
+import 'package:superset/core/models/workout_log.dart';
 
 final class Workout with EquatableMixin {
   Workout({
     this.date,
-    this.exercises,
+    this.logs,
+    this.note,
   });
 
   factory Workout.fromJson(Map<String, dynamic> json) {
     return Workout(
       date: json['date'] as DateTime?,
-      exercises: json['exercises'] as List<Exercise>?,
+      logs: json['logs'] as List<WorkoutLog>?,
+      note: json['note'] as String?,
     );
   }
   final DateTime? date;
-  final List<Exercise>? exercises;
+  final List<WorkoutLog>? logs;
+  final String? note;
 
   @override
   List<Object?> get props => [
     date,
-    exercises,
+    logs,
+    note,
   ];
 
   Workout copyWith({
     DateTime? date,
-    List<Exercise>? exercises,
+    List<WorkoutLog>? logs,
+    String? note,
   }) {
     return Workout(
       date: date ?? this.date,
-      exercises: exercises ?? this.exercises,
+      logs: logs ?? this.logs,
+      note: note ?? this.note,
     );
   }
 }
